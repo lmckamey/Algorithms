821a5494a4aeb3a33c97b045b8f7d3dcd71e3490
Lucas <lmckamey@student.neumont.edu>
2018-04-03 21:52:51 -0600

    Start Project
    Stared the Project and added a few methods and things

---
+++ Lab01-IsomorphicStrings/Lab01-IsomorphicStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01_IsomorphicStrings
+{
+    class Program
+    {
+
+        private static string[] strings;
+
+        static List<String> isomorphicExactList = new List<string>();
+        static List<String> isomorphicLooseList = new List<string>();
+        static List<String> nonisomorphicList = new List<string>();
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Please enter a file path: ");
+            OpenFile(Console.ReadLine());
+            printAllStringsInList();
+        }
+
+        static void OpenFile(string path)
+        {
+            strings = System.IO.File.ReadAllLines(path);  
+        }
+
+        static void printAllStringsInList()
+        {
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.WriteLine(strings[i]);
+            }
+            
+        }
+    }
+}
