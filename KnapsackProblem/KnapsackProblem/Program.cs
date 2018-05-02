4b9ee6340a3f6d225139f726d1cda2d7a6893608
Lucas <lmckamey@student.neumont.edu>
2018-05-02 11:50:35 -0600

    removed zip and worked on knapsack
    Tried to do it the nQueens way, didn't work and now i tried this way and it works better but it still doesn't produce the right results

--- KnapsackProblem/KnapsackProblem/Program.cs
+++ KnapsackProblem/KnapsackProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -15,15 +16,24 @@ namespace KnapsackProblem
     class Program
     {
 
-        static KnapsackItem[] items;
+        static List<KnapsackItem> items = new List<KnapsackItem>();
+        static List<KnapsackItem> solution = new List<KnapsackItem>();
+
+        static List<List<KnapsackItem>> solutions = new List<List<KnapsackItem>>();
+        static int allotedweight;
+
         static void Main(string[] args)
         {
-
+            Run();
         }
 
         static void Run()
         {
-
+            Console.WriteLine("Welcome to the knapsack Simulator!");
+            Console.WriteLine("Please enter a file path: ");
+            OpenFile(Console.ReadLine());
+            PrintList();
+            Console.WriteLine(DeterminSolutions(items.Count - 1));
         }
 
         static void OpenFile(string path)
@@ -31,16 +41,74 @@ namespace KnapsackProblem
             string[] lines = File.ReadLines(path).ToArray();
             int knapsackSize = 0;
             int.TryParse(lines[0], out knapsackSize);
-            items = new KnapsackItem[knapsackSize];
-
-            for (int i = 1; i < lines.Length; i++)
+            allotedweight = knapsackSize;
+            for (int i = 2; i < lines.Length - 1; i++)
             {
-                if(true/* string begins with */)
-                {
-                    lines[i].Split()
-                }
+                if (lines[i].Trim() == "")
+                    break;
+                populateItemsArray(lines[i].Trim());
             }
 
         }
+        static void populateItemsArray(string raw)
+        {
+            KnapsackItem item = new KnapsackItem();
+            string[] split = raw.Split(",");
+            item.name = split[0];
+            int.TryParse(split[1], out item.weight);
+            int.TryParse(split[2], out item.value);
+            items.Add(item);
+        }
+
+
+        static int DeterminSolutions(int index)
+        {
+            int result = 0;
+            if(solution.Contains(items[index]))
+            {
+
+            }
+            if (index == 0 || allotedweight == 0)
+                result = 0;
+            else if (items[index].weight > allotedweight)
+            {
+                result = DeterminSolutions(index - 1);
+            }
+            else
+            {
+                int temp = DeterminSolutions(index - 1);
+                allotedweight -= items[index].weight;
+                int temp2 = items[index].value + DeterminSolutions(index - 1);
+                result = Math.Max(temp, temp2);
+            }
+            return result;
+        }
+        static void PrintList()
+        {
+            Console.WriteLine("ITEMS FOUND: ");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.name + ", " + item.weight + ", " + item.value);
+            }
+
+            //Console.WriteLine("Solution");
+            //foreach (var item in solution)
+            //{
+            //    Console.WriteLine(item.name);
+            //}
+            //Console.WriteLine("Solutions");
+            //foreach (var item in solutions)
+            //{
+            //    foreach (var item2 in item)
+            //    {
+            //        Console.WriteLine(item2.name);
+            //    }
+            //}
+        }
+
+
+
     }
 }
+
+
