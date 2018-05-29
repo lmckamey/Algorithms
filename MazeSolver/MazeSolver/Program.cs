2b42ed7443d266357d980c30126b64cd5886e691
Lucas <lmckamey@student.neumont.edu>
2018-05-28 21:07:47 -0600

    Maze Solver
    Continued Maze Solver, Needs more work

--- MazeSolver/MazeSolver/Program.cs
+++ MazeSolver/MazeSolver/Program.cs
@@ -16,9 +16,8 @@ namespace MazeSolver
         }
 
 
-        static string[] rawFileStrings;
-
-
+       static List<List<string>> mazeInfo = new List<List<string>>();
+        static List<Node> currentMaze = new List<Node>();
         static void Main(string[] args)
         {
             Run();
@@ -26,35 +25,91 @@ namespace MazeSolver
 
         static void Run()
         {
-            ParseFileData();
+            string[] rawFileInfo = null;
+            ParseFileData(ref rawFileInfo);
+            populateMazeInfo(rawFileInfo);
 
-            foreach (var item in rawFileStrings)
-            {
-                Console.WriteLine(item);
-            }
+            PrintMazeInfo();
+
+            GenerateMaze(3);
         }
 
-        static bool ParseFileData()
+        static bool ParseFileData(ref string[] lines)
         {
             string path = "";
             while (path == null || path == "")
             {
                 path = PromptForString("Please Enter a file Path");
             }
-            rawFileStrings = File.ReadAllLines(path);
+            string[] rawFileStrings = File.ReadAllLines(path);
             for (int i = 0; i < rawFileStrings.Length; i++)
             {
                 if (rawFileStrings[i] != "")
                     if (rawFileStrings[i][0] == '/')
                         rawFileStrings[i] = "";
             }
+            lines = rawFileStrings;
             return (rawFileStrings != null);
         }
 
+        static void populateMazeInfo(string[] rawFileInfo)
+        {
+            List<String> info = new List<string>();
+            for (int i = 0; i < rawFileInfo.Length; i++)
+            {
+                if (rawFileInfo[i] != "")
+                {
+                    info.Add(rawFileInfo[i]);
+                }
+                else
+                {
+                    if (info.Count != 0)
+                    {
+                        mazeInfo.Add(new List<string>(info));
+                        info.Clear();
+                    }
+                }
+            }
+        }
+        
         static string PromptForString(string prompt)
         {
             Console.WriteLine(prompt);
             return Console.ReadLine();
+        }
+
+        static void PrintMazeInfo()
+        {
+
+            for (int i = 0; i < mazeInfo.Count; i++)
+            {
+                Console.WriteLine("MAZEINFO " + i + ":");
+                foreach (var item in mazeInfo[i])
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static void GenerateMaze(int index)
+        {
+            foreach (var item in mazeInfo[index])
+            {
+                GenerateNode(item);
+            }
+        }
+
+        static Node GenerateNode(string nodeInfo)
+        {
+            var query = nodeInfo.Where(c => c != ',');
+            var thingy = query.ToList();
+            foreach (var item in thingy)
+            {
+                Console.WriteLine(item);
+            }
+            return null;
         }
     }
 }
